6626583d3fa99090a951cc1e14c71eacba4f63e9
Lucas Farah <lucas.farah@me.com>
2016-01-23 12:32:23 -0800

    some design shit

--- iOS/DevBootcampHackathon/DevBootcampHackathon/ViewController.swift
+++ iOS/DevBootcampHackathon/DevBootcampHackathon/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var lblHello: UILabel!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.lblHello.text = "Fuck you"
   }
 
   override func didReceiveMemoryWarning() {
